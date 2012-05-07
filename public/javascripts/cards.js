4d4534b421694db6d359bc45504cea507700e395
dipesh acharya <dipesh.acharya@gmail.com>
2012-05-07 21:20:49 +0900

    package.json node.js version specified

--- public/javascripts/cards.js
+++ public/javascripts/cards.js
@@ -1,4 +1,4 @@
-var ws_host = window.location.href.replace(/(http|https)(:\/\/[^\/]*)(\:.*)(\/.*)/, '$1$2');
+var ws_host = window.location.href.replace(/(http|https)(:\/\/[^\/]*)(\:.*)(\/.*)/, '$1$2:80');
 
 var User = new function(){
     this.userid='';
